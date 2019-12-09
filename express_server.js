1f4438db5e7745e0c2123796dd4dd05a89ea0d30
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-12-09 20:16:10 +0000

    first template

--- express_server.js
+++ express_server.js
@@ -16,9 +16,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
+
+// app.get("/hello", (req, res) => {
+//   let templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
 
 app.get("/set", (req, res) => {
   const a = 1;

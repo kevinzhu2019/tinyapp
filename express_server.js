547a59cff8db1e297b16e9e76be2b7cb65b16365
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-12-11 01:04:34 +0000

    cookie

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.port || 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const generateRandomString = function() {
@@ -15,6 +17,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req,res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,7 +37,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -42,7 +55,7 @@ app.get("/urls/new", (req, res) => {
 //above route must be above "/urls/:shortURL" since otherwise it would be taken as :ID
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]}; //passing username to this template so that _header can get the username as well.
   res.render("urls_show", templateVars);
 });
 
@@ -51,6 +64,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   // let templateVars = urlDatabase;
   // res.render("urls_index", templateVars);
   res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls/");
 });
 
 app.get("/u/:shortURL", (req, res) => {
